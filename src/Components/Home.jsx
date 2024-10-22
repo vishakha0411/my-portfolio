07564010820d15b04b4cba3d594ae613706d86f3
Vishakha Gaurkar <82572740+vishakha0411@users.noreply.github.com>
2024-10-22 09:34:54 +0000

    Customize portfolio

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -28,7 +28,7 @@ const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ color: "white", position: "absolute", top: "5rem", left: "10rem", width: "20rem" }}>
+      <div style={{ color: "white", position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
